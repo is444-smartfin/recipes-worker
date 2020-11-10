c5153e07a917e270eb508c2bf9841deea6f1c486
Goi Jia Jian <jiajiannn@gmail.com>
2020-11-11 01:16:20 +0800

    fix: update

--- app.js
+++ app.js
@@ -1,92 +1,107 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch')
 
 exports.handler = async (event, context) => {
-  const eventRecord = event.Records[0];
-  const eventName = eventRecord.eventName;
-  const dynamodbRecord = eventRecord.dynamodb;
-  console.log("event", eventRecord);
+  const eventRecord = event.Records[0]
+  const eventName = eventRecord.eventName
+  const dynamodbRecord = eventRecord.dynamodb
+  console.log('event', eventRecord)
 
   // If userIdentity prop exists in event.Records[0], means it's a System Delete (based off TTL attribute)
-  if ("userIdentity" in eventRecord && eventName === "REMOVE") {
+  if ('userIdentity' in eventRecord && eventName === 'REMOVE') {
     // We immediately process it
-    const email = dynamodbRecord.OldImage.email.S;
-    const taskName = dynamodbRecord.OldImage.task_name.S;
-    const eventId = eventRecord.eventID;
-    console.log("Currently running the task", taskName, "for user", email);
+    const email = dynamodbRecord.OldImage.email.S
+    const taskName = dynamodbRecord.OldImage.task_name.S
+    const eventId = eventRecord.eventID
+    console.log('Currently running the task', taskName, 'for user', email)
 
-    if (taskName === "tbank.salary.transfer") {
-      const apiKey = "randomKey";
-      const payload = dynamodbRecord.OldImage;
+    if (taskName === 'tbank.salary.transfer') {
+      const apiKey = 'randomKey'
+      const payload = dynamodbRecord.OldImage
       const data = JSON.stringify({
         apiKey,
         eventId,
-        payload,
-      });
-      console.log(data);
+        payload
+      })
+      console.log(data)
       const resp = await post(
-        "https://api.ourfin.tech/integrations/tbank/recipe_salary_transfer",
+        'https://api.ourfin.tech/integrations/tbank/recipe_salary_transfer',
         {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json'
         },
         data
-      );
-    } else {
-      console.log("Nothing to run at the moment for task", taskName, "for user", email);
+      )
+      console.log(resp)
+    } else if (taskName === 'smartfin.aggregated_email') {
+      const apiKey = 'randomKey'
+      const payload = dynamodbRecord.OldImage
+      const data = JSON.stringify({
+        apiKey,
+        eventId,
+        payload
+      })
+      console.log(data)
+      const resp = await post(
+        'https://api.ourfin.tech/integrations/smartfin/aggregated_email',
+        {
+          'Content-Type': 'application/json'
+        },
+        data
+      )
+      console.log(resp)
     }
 
-    console.log(resp);
-    console.log("Done running the task", taskName, "for user", email);
+    console.log('Done running the task', taskName, 'for user', email)
   } else {
     console.log(
-      "Event is",
+      'Event is',
       eventName,
-      "and not a system delete, not running the task."
-    );
+      'and not a system delete, not running the task.'
+    )
   }
 
   const response = {
     statusCode: 200,
     body: JSON.stringify(
       '{"status": 200, "message": "Lambda task has been triggered."'
-    ),
-  };
-  return response;
-};
+    )
+  }
+  return response
+}
 
 const get = async (requestUrl) => {
   return new Promise((resolve, reject) => {
     try {
       fetch(requestUrl, {
-        method: "GET",
+        method: 'GET',
         headers: {
-          Accept: "application/json",
-        },
+          Accept: 'application/json'
+        }
       })
         .then((response) => response.json())
-        .then((json) => resolve(json));
+        .then((json) => resolve(json))
     } catch (error) {
-      reject(error);
+      reject(error)
     }
-  });
-};
+  })
+}
 
-const post = async (requestUrl, headers = {}, body = "") => {
+const post = async (requestUrl, headers = {}, body = '') => {
   return new Promise((resolve, reject) => {
-    console.log(headers);
+    console.log(headers)
     try {
       fetch(requestUrl, {
-        method: "POST",
+        method: 'POST',
         headers: {
-          Accept: "application/json",
-          ...headers,
+          Accept: 'application/json',
+          ...headers
         },
-        body,
+        body
       })
         .then((response) => response.json())
-        .then((json) => resolve(json));
+        .then((json) => resolve(json))
     } catch (error) {
-      console.log("post > error occured");
-      reject(error);
+      console.log('post > error occured')
+      reject(error)
     }
-  });
-};
+  })
+}
