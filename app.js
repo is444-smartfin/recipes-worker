bc1273301a5780036fb4a971fe8675cf24ec139b
Goi Jia Jian <jiajiannn@gmail.com>
2020-11-15 17:00:37 +0800

    feat: aggregated email function

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const fetch = require('node-fetch')
 
-exports.handler = async (event, context) => {
+exports.tbank_salary_transfer = async (event, context) => {
   const eventRecord = event.Records[0]
   const eventName = eventRecord.eventName
   const dynamodbRecord = eventRecord.dynamodb
   console.log('event', eventRecord)
 
   // If userIdentity prop exists in event.Records[0], means it's a System Delete (based off TTL attribute)
-  if ('userIdentity' in eventRecord && eventName === 'REMOVE') {
+  if (eventName === 'REMOVE') {
+  // if ('userIdentity' in eventRecord && eventName === 'REMOVE') {
     // We immediately process it
     const email = dynamodbRecord.OldImage.email.S
     const taskName = dynamodbRecord.OldImage.task_name.S
@@ -24,14 +25,50 @@ exports.handler = async (event, context) => {
       })
       console.log(data)
       const resp = await post(
-        'https://api.ourfin.tech/integrations/tbank/recipe_salary_transfer',
+        'https://jiajian-602166a7.localhost.run/integrations/tbank/recipe_salary_transfer',
+        // 'https://api.ourfin.tech/integrations/tbank/recipe_salary_transfer',
         {
           'Content-Type': 'application/json'
         },
         data
       )
       console.log(resp)
-    } else if (taskName === 'smartfin.aggregated_email') {
+    }
+
+    console.log('Done running the task', taskName, 'for user', email)
+  } else {
+    console.log(
+      'Event is',
+      eventName,
+      'and not a system delete, not running the task.'
+    )
+  }
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(
+      '{"status": 200, "message": "Lambda task has been triggered."'
+    )
+  }
+  return response
+}
+
+exports.smartfin_aggregated_email = async (event, context) => {
+  const eventRecord = event.Records[0]
+  const eventName = eventRecord.eventName
+  const dynamodbRecord = eventRecord.dynamodb
+  console.log('event', eventRecord)
+
+  // If userIdentity prop exists in event.Records[0], means it's a System Delete (based off TTL attribute)
+  if (eventName === 'REMOVE') {
+  // if ('userIdentity' in eventRecord && eventName === 'REMOVE') {
+    // We immediately process it
+    const email = dynamodbRecord.OldImage.email.S
+    const taskName = dynamodbRecord.OldImage.task_name.S
+    const eventId = eventRecord.eventID
+    console.log('Currently running the task', taskName, 'for user', email)
+
+    if (taskName === 'smartfin.aggregated_email') {
       const apiKey = 'randomKey'
       const payload = dynamodbRecord.OldImage
       const data = JSON.stringify({
